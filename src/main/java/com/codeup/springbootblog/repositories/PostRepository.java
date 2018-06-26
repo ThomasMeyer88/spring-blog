9c96cbdf616b4f367bbcefafe4594f8a8a84b292
Thomas <trexmeyer@gmail.com>
2018-06-26 12:13:56 -0500

    removed unused imports

--- src/main/java/com/codeup/springbootblog/repositories/PostRepository.java
+++ src/main/java/com/codeup/springbootblog/repositories/PostRepository.java
@@ -1,13 +1,10 @@
 package com.codeup.springbootblog.repositories;
 
 import com.codeup.springbootblog.models.Post;
-import org.springframework.data.domain.Page;
 import org.springframework.data.repository.CrudRepository;
 
 import java.awt.print.Pageable;
 import java.util.List;
-
-import static javafx.scene.input.KeyCode.T;
 
 public interface PostRepository extends CrudRepository<Post, Long> {
     Post findByTitle(String Title);
--- src/main/java/com/codeup/springbootblog/repositories/Users.java
+++ src/main/java/com/codeup/springbootblog/repositories/Users.java
@@ -1,14 +1,10 @@
 package com.codeup.springbootblog.repositories;
 
-import com.codeup.springbootblog.models.Post;
 import com.codeup.springbootblog.models.User;
-import org.springframework.data.domain.Page;
 import org.springframework.data.repository.CrudRepository;
 
-import java.awt.print.Pageable;
 import java.util.List;
 
-import static javafx.scene.input.KeyCode.T;
 
 public interface Users extends CrudRepository<User, Long> {
     User findByEmail(String email);
