cda32d7079f5ccf7dcedb72b98d2eb319a20fe7a
Thomas <trexmeyer@gmail.com>
2018-06-26 15:57:40 -0500

    stuff

--- src/main/java/com/codeup/springbootblog/controllers/HomeController.java
+++ src/main/java/com/codeup/springbootblog/controllers/HomeController.java
@@ -11,7 +11,7 @@ class HomeController {
     @GetMapping("/")
     @ResponseBody
     public String home() {
-        return "redirect:/minesweeper";
+        return "/minesweeper";
     }
 
     @GetMapping("/hello/{name}")
